31264a690dd11767fdb3d09a9313812dd6a5f1e9
maendeleolab <maendeleolab@gmail.com>
2023-10-28 22:33:22 +0000

    added more codes to the vpcs_template

--- network_prerequisites/vpcs_config.py
+++ network_prerequisites/vpcs_config.py
@@ -2,15 +2,6 @@
 
 from vpcs_template import * 
 
-
-describe_resources('boto3_vpc1')
-print('\nPrinting next item...\n')
-describe_resources('NetworkDevEgress')
-print('\nPrinting all items...\n')
-describe_some()
-print('\nPrinting item id...\n')
-print(describe_ids('boto3_vpc1'))
-print('\nCreating vpc...\n')
-create_resources('boto3_vpc1','10.100.0.0/20')
-print('\nCreating another vpc...\n')
-create_resources('boto3_vpc2','10.200.0.0/20')
+#Create vpcs
+create_resources('boto3_vpc1','10.10.0.0/20')
+create_resources('boto3_vpc2','10.20.0.0/20')
--- network_prerequisites/vpcs_template.py
+++ network_prerequisites/vpcs_template.py
@@ -6,7 +6,7 @@ import boto3, sys
 # the ec2 service in aws
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
-# This is a function to describe resources
+# This is a function to describe resources tag names
 def describe_resources(name):
   try:
     resources = ec2.describe_vpcs(
@@ -21,90 +21,104 @@ def describe_resources(name):
     )
     for item in resources['Vpcs'][0]['Tags']:
       return item['Value']
-      #print(resources)
-  except Exception as err:
-      print(f'Error found: {err}...')
 
+  except Exception as err:
+      print(f'Resource {name} does not exist. Error found: {err}...')
+
+# This is a function to describe all the resources
 def describe_some():
   resources = ec2.describe_vpcs(
   )
-  print(resources)
-#describe_some()
+  return resources
+  #describe_some()
 
 # This is a function to describe the resources ids
 def describe_ids(name):
-  resources = ec2.describe_vpcs(
-      Filters=[
-	  {
-	      'Name': 'tag:Name',
-	      'Values': [
-		  name,
-	      ]
-	  },
-      ]
-  )
-  for item in resources['Vpcs']:
-      return item['VpcId']
-         
-
-#describe_ids()
-
-# This is a function to create resources
-def create_resources(name, cidr):
-  results = describe_resources(name)
-  if results == name:
-    print('resource already exists...')
-    pass
-  elif results != name:
-    resources = ec2.create_vpc(
-        CidrBlock=cidr,
-        #AmazonProvidedIpv6CidrBlock=True|False,
-        #Ipv6Pool='string',
-        #Ipv6CidrBlock='string',
-        #Ipv4IpamPoolId='string',
-        #Ipv4NetmaskLength=123,
-        #Ipv6IpamPoolId='string',
-        #Ipv6NetmaskLength=123,
-        #DryRun=True|False,
-        InstanceTenancy='default',
-        #Ipv6CidrBlockNetworkBorderGroup='string',
-        TagSpecifications=[
+  try:
+    resources = ec2.describe_vpcs(
+        Filters=[
             {
-                'ResourceType': 'vpc',
-                'Tags': [
-                    {
-                        'Key': 'Name',
-                        'Value': name
-                    },
+                'Name': 'tag:Name',
+                'Values': [
+                    name,
                 ]
             },
         ]
     )
-    return resources
+    for item in resources['Vpcs']:
+        return item['VpcId']
+  except Exception as err:
+    print(f'Unable to describe vpc. See error {err}...')
+         
+#describe_ids()
+
+# This is a function to create resources
+def create_resources(name, cidr):
+  try:
+    results = describe_resources(name)
+    if results == name:
+      print(f'{name} already exists...')
+      pass
+    elif results != name:
+      resources = ec2.create_vpc(
+          CidrBlock=cidr,
+          #AmazonProvidedIpv6CidrBlock=True|False,
+          #Ipv6Pool='string',
+          #Ipv6CidrBlock='string',
+          #Ipv4IpamPoolId='string',
+          #Ipv4NetmaskLength=123,
+          #Ipv6IpamPoolId='string',
+          #Ipv6NetmaskLength=123,
+          #DryRun=True|False,
+          InstanceTenancy='default',
+          #Ipv6CidrBlockNetworkBorderGroup='string',
+          TagSpecifications=[
+              {
+                  'ResourceType': 'vpc',
+                  'Tags': [
+                      {
+                          'Key': 'Name',
+                          'Value': name
+                      },
+                  ]
+              },
+          ]
+      )
+      return resources
+  except Exception as err:
+    print(f'Found error: {err}...')
 
 #create_resources()
 
 # This function modifies resources attributes
-def modify_resources(resource):
-  resources = ec2.modify_vpc_attribute(
-      EnableDnsHostnames={
-          'Value': True #True|False
-      },
-      EnableDnsSupport={
-          'Value': True #True|False
-      },
-      VpcId=resource,
-      EnableNetworkAddressUsageMetrics={
-          'Value': True #True|False
-      }
-  )
+def modify_resources(resource, value):
+  try:
+    resources = ec2.modify_vpc_attribute(
+        EnableDnsHostnames={
+            'Value': value #True|False
+        },
+        EnableDnsSupport={
+            'Value': value #True|False
+        },
+        VpcId=resource,
+        EnableNetworkAddressUsageMetrics={
+            'Value': value #True|False
+        }
+    )
+  except Exception as err:
+    print(f'Error found: {resource}...')
+
 #modify_resources(describe_ids())
 
 # This is a function to delete resources
 def delete_resources(resource):
-  resources = ec2.delete_vpc(
-      VpcId=resource,
-      #DryRun=True|False
-  )
+  try:
+    resources = ec2.delete_vpc(
+        VpcId=resource,
+        #DryRun=True|False
+    )
+  except Exception as err:
+    print(f'Error found: {err}...')
+
 
 #delete_resources(describe_ids())
