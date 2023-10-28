38d0f21c03a05406efd8b4386157dbce92f00711
maendeleolab <maendeleolab@gmail.com>
2023-10-28 12:57:08 +0000

    added new code

--- network_prerequisites/vpc_config.py
+++ network_prerequisites/vpc_config.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-from vpcs_template import describe_resources, describe_ids, describe_some
-from vpcs_template import create_resources
+from vpcs_template import * 
 
 
-#describe_resources('boto3_vpc1')
-#print('\nPrinting next item...\n')
-#describe_resources('NetworkDevEgress')
-#print('\nPrinting all items...\n')
-#describe_some()
-#print('\nPrinting item id...\n')
-#print(describe_ids('boto3_vpc1'))
+describe_resources('boto3_vpc1')
+print('\nPrinting next item...\n')
+describe_resources('NetworkDevEgress')
+print('\nPrinting all items...\n')
+describe_some()
+print('\nPrinting item id...\n')
+print(describe_ids('boto3_vpc1'))
 print('\nCreating vpc...\n')
 create_resources('boto3_vpc1','10.100.0.0/20')
 print('\nCreating another vpc...\n')
