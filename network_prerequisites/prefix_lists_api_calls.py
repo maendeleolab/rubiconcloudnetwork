482695b7d6099ed7c106f9f5130ece9f5367f494
maendeleolab <maendeleolab@gmail.com>
2023-11-04 13:26:09 -0400

    added more code to prefix list functions

--- network_prerequisites/prefix_lists_api_calls.py
+++ network_prerequisites/prefix_lists_api_calls.py
@@ -23,6 +23,20 @@ def get_prefixlist_name(prefixlist, ec2):
   except Exception as err:
     print(f'Error found: {err}...')
 
+# This function checks if cidr entry already exists in prefix-list
+def verify_if_cidr_entry_exists(prefixlist_id, cidr_entry, ec2):
+  try:
+    resources = ec2.get_managed_prefix_list_entries(
+    #DryRun=True|False,
+    PrefixListId=prefixlist_id,
+    )
+    for entry in resources['Entries']:
+      if entry['Cidr'] == cidr_entry:
+        print(f'Cidr : {entry["Cidr"]}')
+        return entry['Cidr']
+  except Exception as err:
+    print(f'Error found: {err}...')
+
 # This function returns the prefix list id
 def get_prefixlist_id(prefixlist_id, ec2):
   try:
@@ -32,14 +46,6 @@ def get_prefixlist_id(prefixlist_id, ec2):
   else:
     resources = ec2.describe_managed_prefix_lists(
 	#DryRun=True|False,
-	Filters=[
-	    {
-		'Name': 'tag:Name',
-		'Values': [
-		    prefixlist_id,
-		]
-	    },
-	],
     )
     print(f'Prefix-list name: {resources['PrefixLists']['PrefixListId']}')
     return resources['PrefixLists']['PrefixListId']
@@ -47,59 +53,64 @@ def get_prefixlist_id(prefixlist_id, ec2):
     print(f'Error found: {err}...')
 
 # This function creates a prefix list for ipv4 addresses
-def create_prefixlist(name, entry, max_entries, ec2):
-  resources = ec2.create_managed_prefix_list(
-      # DryRun=True|False,
-      PrefixListName=name,
-      Entries=[
-	  {
-	      'Cidr': entry,
-	      'Description': name
-	  },
-      ],
-      MaxEntries=max_entries,
-      TagSpecifications=[
-	  {
-	      'ResourceType': 'prefix-list',
-	      'Tags': [
-		  {
-		      'Key': 'Name',
-		      'Value': name
-		  },
-	      ]
-	  },
-      ],
-      AddressFamily='IPv4',
-  )
+def create_prefixlist(name, cidr_entry, max_entries, ec2):
+  try:
+    if name == get_prefixlist_name(name, ec2):
+      print(f"Prefix-list {name} doesn't exist...")
+      pass
+    else:
+      print(f'Creating prefix-list {name}...')
+      resources = ec2.create_managed_prefix_list(
+          # DryRun=True|False,
+          PrefixListName=name,
+          Entries=[
+          {
+              'Cidr': cidr_entry,
+              'Description': name
+          },
+          ],
+          MaxEntries=max_entries,
+          TagSpecifications=[
+          {
+              'ResourceType': 'prefix-list',
+              'Tags': [
+              {
+                  'Key': 'Name',
+                  'Value': name
+              },
+              ]
+          },
+          ],
+          AddressFamily='IPv4',
+      )
+  except Exception as err:
+    print(f'Error found: {err}...')
 
 # This function adds new entries to the prefix list
-def add_entries_to_prefixlist(prefixlist, cidr_entry, description, ec2):
-  resources = ec2.modify_managed_prefix_list(
-      #DryRun=True|False,
-      PrefixListId=prefixlist,
-      #CurrentVersion=123,
-      #PrefixListName='string',
-      AddEntries=[
-	  {
-	      'Cidr': cidr_entry,
-	      'Description': description
-	  },
-      ],
-  )
-
-# This function removes entries from the prefix list
-def remove_entries_from_prefixlist(prefixlist, cidr_entry, ec2):
-  resources = ec2.modify_managed_prefix_list(
-      #DryRun=True|False,
-      PrefixListId=prefixlist,
-      #CurrentVersion=123,
-      #PrefixListName='string',
-      RemoveEntries=[
-	  {
-	      'Cidr': cidr_entry
-	  },
-      ],
-  )
+def add_entries_to_prefixlist(prefixlist_id, cidr_entry, description, ec2):
+  try:
+    if get_prefixlist_name == prefixlist_id:
+      print(f"Prefix-list {prefixlist_id} does'nt exist...")
+      pass
+    elif verify_if_cidr_entry_exists(prefixlist_id, cidr_entry, ec2) == cidr_entry:
+      print(f'Cidr {cidr_entry} already exists...')
+      pass
+    else:
+      print(f'Adding Cidr {cidr_entry} to prefix-list {prefixlist}...')
+      resources = ec2.modify_managed_prefix_list(
+          #DryRun=True|False,
+          PrefixListId=prefixlist_id,
+          #CurrentVersion=123,
+          #PrefixListName='string',
+          AddEntries=[
+          {
+              'Cidr': cidr_entry,
+              'Description': description
+          },
+          ],
+      )
+  except Exception as err:
+    print(f'Error found: {err}...')
 
 # This function removes entries from the prefix list
 def remove_entries_from_prefixlist(prefixlist, cidr_entry, ec2):
