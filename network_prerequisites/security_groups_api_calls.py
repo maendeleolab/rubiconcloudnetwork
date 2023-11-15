105a6a412031b1a232a5ec98644fa760e393f62c
maendeleolab <maendeleolab@gmail.com>
2023-11-15 00:20:12 -0500

    added code to security-group  script

--- network_prerequisites/security_groups_api_calls.py
+++ network_prerequisites/security_groups_api_calls.py
@@ -1,25 +1,107 @@
 #!/usr/bin/env python3
 
 
+# This function gets the security-group id
+
+# This function gets the name of the security-group
+
 # This function creates a security-group
-def create_sg(sg_name, ec2):
-try:
-resources = ec2.create_security_group(
-    Description='string',
-    GroupName='string',
-    VpcId='string',
-    TagSpecifications=[
-        {
-            'ResourceType':'security-group',
-            'Tags': [
-                {
-                    'Key': 'string',
-                    'Value': 'string'
-                },
-            ]
-        },
-    ],
-    #DryRun=True|False
-)
-except Exception as err:
-print(f'Error found: {err}...')
+def create_sg(sg_name, vpc_id, ec2):
+  try:
+    resources = ec2.create_security_group(
+        Description=sg_name,
+        GroupName=sg_name,
+        VpcId=vpc_id,
+        TagSpecifications=[
+            {
+                'ResourceType':'security-group',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': sg_name
+                    },
+                ]
+            },
+        ],
+        #DryRun=True|False
+    )
+    print(f'Creating security-group: {sg_name}...')
+  except Exception as err:
+    print(f'Error found: {err}...')
+
+# This function adds egress entries to a security-group
+def add_egress_sg(sg_group_id, 
+                  from_port, 
+                  protocol_number, 
+                  cidr_entry, 
+                  description, 
+                  prefixlist_id, 
+                  to_port, 
+                  ec2
+                  ):
+  try:
+    resources = ec2.authorize_security_group_egress(
+        #DryRun=True|False,
+        GroupId=sg_group_id,
+        IpPermissions=[
+            {
+                'FromPort': from_port,
+                'IpProtocol': protocol_number,
+                'PrefixListIds': [
+                    {
+                        'Description': description,
+                        'PrefixListId': prefixlist_id
+                    },
+                ],
+                'ToPort': to_port,
+                    },
+                ]
+            },
+        ],
+    )
+    print(f'Adding egress rule to security-group: {sg_group_id}...')
+  except Exception as err:
+    print(f'Error found: {err}...')
+
+# This function adds ingress entries to a security-group
+def add_ingress_sg(sg_group_id, 
+                  from_port, 
+                  protocol_number, 
+                  cidr_entry, 
+                  description, 
+                  prefixlist_id, 
+                  to_port, 
+                  ec2
+                  ):
+  try:
+    resources = ec2.authorize_security_group_ingress(
+        #DryRun=True|False,
+        GroupId=sg_group_id,
+        IpPermissions=[
+            {
+                'FromPort': from_port,
+                'IpProtocol': protocol_number,
+                'PrefixListIds': [
+                    {
+                        'Description': description,
+                        'PrefixListId': prefixlist_id
+                    },
+                ],
+                'ToPort': to_port,
+                    },
+                ]
+            },
+        ],
+    )
+    print(f'Adding ingress rule to security-group: {sg_group_id}...')
+  except Exception as err:
+    print(f'Error found: {err}...')
+
+
+# This function revokes the egress security-group
+
+# This function deletes a security group
+
+
+
+
