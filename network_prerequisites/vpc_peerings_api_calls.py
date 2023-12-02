69f5f842aa953b819cd67c34efc3af9d712aeedc
maendeleolab <maendeleolab@gmail.com>
2023-12-01 19:26:15 -0500

    added more code for vpc peering

--- network_prerequisites/vpc_peerings_api_calls.py
+++ network_prerequisites/vpc_peerings_api_calls.py
@@ -2,22 +2,69 @@
 
 
 
-def get_vpc_peering_id(name, ec2):
-resources = ec2.describe_vpc_peering_connections(
-    Filters=[
-        {
-            'Name': 'tag:Name',
-            'Values': [
-                name,
-            ]
-        },
-    ],
-    #DryRun=True|False,
-    MaxResults=200
-)
+def get_vpc_peering_connection_id(name, ec2):
+	try:
+		resources = ec2.describe_vpc_peering_connections(
+				Filters=[
+						{
+								'Name': 'tag:Name',
+								'Values': [
+										name,
+								]
+						},
+				],
+				#DryRun=True|False,
+				#MaxResults=200
+		)
+		print(f'Peering connection id:
+		{resources["VpcPeeringConnections"]["VpcPeeringConnectionId"]')
+		return resources["VpcPeeringConnections"]["VpcPeeringConnectionId"]
+	except Exception as err:
+		print(f'Error found: {err}...')
 
 
+def get_vpc_peering_connection_name(name, ec2):
+	try:
+		resources = ec2.describe_vpc_peering_connections(
+				Filters=[
+						{
+								'Name': 'tag:Name',
+								'Values': [
+										name,
+								]
+						},
+				],
+				#DryRun=True|False,
+				#NextToken='string'
+				#MaxResults=200
+		)
+		for item in resources['VpcPeeringConnections'][0]['Tags']:
+			print(f'Vpc peering: {item["Value"]}')
+			return item["Value"]
+	except Exception as err:
+		print(f'Error found: {err}...')
 
+
+def get_vpc_peering_connection_status(name, ec2):
+	try:
+		resources = ec2.describe_vpc_peering_connections(
+				Filters=[
+						{
+								'Name': 'tag:Name',
+								'Values': [
+										name,
+								]
+						},
+				],
+				#DryRun=True|False,
+				#NextToken='string'
+				#MaxResults=200
+		)
+		for item in resources['VpcPeeringConnections'][0]['Status']:
+			print(f'Vpc peering: {item["Code"]}')
+			return item["Code"]
+	except Exception as err:
+		print(f'Error found: {err}...')
 
 
 response = client.accept_vpc_peering_connection(
